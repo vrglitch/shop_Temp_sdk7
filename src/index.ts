c767b4425db3819d74ac857130e7141cb9ab8609
vrglitch <wildcube@gmail.com>
2023-12-15 16:34:46 -0300

    Shop models

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// We define the empty imports so the auto-complete feature works as expected.
+import { } from '@dcl/sdk/math'
+import { Animator, AudioSource, AvatarAttach, GltfContainer, Transform, VisibilityComponent, engine, pointerEventsSystem, Material, VideoPlayer } from '@dcl/sdk/ecs'
+import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
+
+initAssetPacks(engine, pointerEventsSystem, {
+  Animator,
+  AudioSource,
+  AvatarAttach,
+  Transform,
+  VisibilityComponent,
+  GltfContainer,
+  Material,
+  VideoPlayer
+})
+
+export function main() {
+  
+
+}
